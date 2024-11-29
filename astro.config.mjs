dac96773be427356e143820e9a5a98218b5ddd7a
jctalavan <jctalavancanelo@gmail.com>
2024-11-29 16:04:52 +0100

    Add a RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://tourmaline-truffle-8a44ed.netlify.app/"
+  });
