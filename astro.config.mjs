330c20bb0b9733d7d77036ad95f05edcd36ec9de
jctalavan <jctalavancanelo@gmail.com>
2024-11-29 17:02:52 +0100

    Add astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tourmaline-truffle-8a44ed.netlify.app/"
-  });
+  site: "https://tourmaline-truffle-8a44ed.netlify.app/",
+  integrations: [preact()]
+});
